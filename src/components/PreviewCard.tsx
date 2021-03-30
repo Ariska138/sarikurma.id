9aa3b0a2dec30070f6f89cb374a1227994dd7619
Ariska Hidayat <ariska138@gmail.com>
2021-03-30 21:25:29 +0700

    add new article

--- src/components/PreviewCard.tsx
+++ src/components/PreviewCard.tsx
@@ -36,7 +36,7 @@ const useStyles = makeStyles((theme) => ({
     [theme.breakpoints.up('xl')]: {
       width: 450,
     },
-    height: 430,
+    height: 460,
     margin: 'auto',
     position: 'relative',
     cursor: 'pointer',
