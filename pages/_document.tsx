c63aa9ebd37ca03730acc99fb8570f3d530c0825
Ariska Hidayat <ariska138@gmail.com>
2021-04-02 05:40:32 +0700

    add google tag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,6 +6,11 @@ import { NAME } from '../src/types/constants'
 
 export default class MyDocument extends Document {
   render(): React.ReactElement {
+    const hidden = {
+      display: 'none',
+      Visibility: 'hidden',
+    }
+
     return (
       <Html lang="en">
         <Head>
@@ -39,8 +44,26 @@ export default class MyDocument extends Document {
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Roboto+Slab:300,400,500,700&display=swap"
           />
+          {/* tag Manager */}
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-WXMHDGR');`,
+            }}
+          />
         </Head>
         <body>
+          <noscript>
+            <iframe
+              src="https://www.googletagmanager.com/ns.html?id=GTM-WXMHDGR"
+              height="0"
+              width="0"
+              style={hidden}
+            ></iframe>
+          </noscript>
           <Main />
           <NextScript />
         </body>
